593ecb38489cb75ceff516798655222497259838
momomomomomomomomomomomomo <142402844+momomomomomomomomomomomomo@users.noreply.github.com>
2025-05-27 12:33:35 +0300

    add the core stats

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useEffect, useReducer } from "react";
+import Header from "./Header.js";
+import Main from "./Main.js";
+import Loader from "./Loader.js";
+import Error from "./Error.js";
+import StartScreen from "./StartScreen.js";
+const initialState = { status: "loading", data: [] };
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, data: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("app failed");
+  }
+}
+export default function App() {
+  const [{ status, data }, dispatch] = useReducer(reducer, initialState);
+  const numQuestions = data.length;
+  useEffect(function () {
+    async function loadData() {
+      try {
+        const res = await fetch("http://localhost:9000/questions");
+        const data = await res.json();
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+      }
+    }
+    loadData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+      </Main>
     </div>
   );
 }
-
-export default App;
